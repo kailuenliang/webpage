e9c94a27814780ab075c498f068794a5dded76b8
Kai-Luen Liang <speak.soul@gmail.com>
2019-11-04 00:39:34 -0800

    REDID header and some other stuff...big change!!!!

--- sketch.js
+++ sketch.js
@@ -10,7 +10,7 @@ function setup() {
   canvas = createCanvas(windowWidth,windowHeight);
   canvas.position(0,0);
   canvas.style('z-index','-1');
-  background(255)
+  background(255);
 }
 
 function draw() {
@@ -25,8 +25,8 @@ var spaceX = map(mouseX,0,width,0,50);
 if((count%2) == 0){
     for(var x = 0; x < width; x+= random(200)){
       noStroke();
-      fill(100,100,random(100,255),random(200));
-      ellipse(x,int(random(height)),random(10));
+      fill(0,0,random(255),random(200));
+      ellipse(x,int(random(height)),random(7));
 
       //  stroke(255,random(10));
       //  line(0,mouseY,mouseX,height/2);
@@ -34,14 +34,14 @@ if((count%2) == 0){
     }
     for(var y = 0 ; y < height; y+=random(200)){
         noStroke();
-        fill(200,30,random(100,200),random(100));
-       ellipse(int(random(width)),y,int(random(10)));
+        fill(0,random(100));
+       ellipse(int(random(width)),y,int(random(2)));
     }
 } else{
       for(var x = 0; x < width; x+=100){
         noStroke();
-        fill(0,random(20));
-        rect(x,int(random(height)),int(random(20)),5);
+        fill(0,random(200));
+        rect(x,int(random(height)),int(random(20)),2);
       //  stroke(200,150,random(0,100),2);
         //line(width,mouseY,mouseX,height/2);
       //rect(random(width),x,random(50));
@@ -52,7 +52,7 @@ if((count%2) == 0){
 
   var ranNum = int(random(200));
   if(ranNum == 6){
-    background(255,255,255);
+    background(255);
 
   }
   if(ranNum < 10){
@@ -64,6 +64,6 @@ if((count%2) == 0){
 
 function mouseClicked() {
 
-    background(255,255,255,200);
+    background(255,200);
 
 }
