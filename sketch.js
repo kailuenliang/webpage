c13032f3c7f7a4dcd2f6b24da97c3fd9f896b22e
Kai-Luen Liang <speak.soul@gmail.com>
2019-11-11 01:53:24 -0800

    REDONE_changed_main_page
    changed main page and some p5js sketch

--- sketch.js
+++ sketch.js
@@ -1,6 +1,14 @@
 let count = 0;
 var canvas;
 
+var tileCount = 10;
+var actRandomSeed = 0; // noise seed
+
+var rectSize = 2;
+
+var randomRange =5;
+
+
 function windowResized(){
   resizeCanvas(windowWidth,windowHeight);
   console.log("resized");
@@ -8,62 +16,109 @@ function windowResized(){
 function setup() {
   // put setup code here
   canvas = createCanvas(windowWidth,windowHeight);
-  canvas.position(0,0);
+
+  var x = (windowWidth - width) / 2;
+var y = (windowHeight - height) / 2;
+  //canvas.position(0, 0); dont want absolute position
+
+  canvas.style('display', 'block');
+  //canvas.position(0,0);
+
   canvas.style('z-index','-1');
-  background(255);
+  //canvas.parent("home-container");
+  background(0);
 }
 
+
+
 function draw() {
-  frameRate(24);
-  // put drawing code here
-  noStroke();
+// background(0);
 
-var spaceX = map(mouseX,0,width,0,50);
+  strokeWeight(1);
+
+  var c1 = color(220);
+  var c2 = color(255);
+  var c3 = color(0);
+
+  randomSeed(actRandomSeed);
+
+  for (var gridY = 0; gridY < tileCount; gridY++) {
+    for (var gridX = 0; gridX < tileCount; gridX++) {
+      var posX = gridX * width / tileCount;
+      var posY = gridY * height / tileCount;
+
+      var shiftX = random(0,height) / tileCount * random(-randomRange, randomRange);
+      var shiftY = random(0,width) / tileCount * random(-randomRange, randomRange);
+      var shiftX2 = mouseX / tileCount * random(-randomRange, randomRange);
+      var shiftY2 = mouseY / tileCount * random(-randomRange, randomRange);
+      var shiftX3 = mouseX / tileCount * random(-randomRange, randomRange);
+      var shiftY3 = mouseY / tileCount * random(-randomRange, randomRange);
+
+      var shiftX4 = mouseX / tileCount * random(-randomRange, randomRange);
+      var shiftY4 = mouseY / tileCount * random(-randomRange, randomRange);
+
+      var shiftX5 = mouseX / tileCount * random(-randomRange, randomRange);
+      var shiftY5 = mouseY / tileCount * random(-randomRange, randomRange);
+            var shiftX6 = mouseX / tileCount * random(-randomRange, randomRange);
+      var shiftY6 = mouseY / tileCount * random(-randomRange, randomRange);
 
 
+      var toggle = int(random(0,3));
 
-if((count%2) == 0){
-    for(var x = 0; x < width; x+= random(200)){
-      noStroke();
-      fill(0,0,random(255),random(200));
-      ellipse(x,int(random(height)),random(7));
+      if(toggle == 0){
+        fill(c1);
+        stroke(255);
+      }
 
-      //  stroke(255,random(10));
-      //  line(0,mouseY,mouseX,height/2);
+      if(toggle == 1){
+         fill(c2);
+        //stroke(c1);
+      }
 
-    }
-    for(var y = 0 ; y < height; y+=random(200)){
-        noStroke();
-        fill(0,random(100));
-       ellipse(int(random(width)),y,int(random(2)));
-    }
-} else{
-      for(var x = 0; x < width; x+=100){
-        noStroke();
-        fill(0,random(200));
-        rect(x,int(random(height)),int(random(20)),2);
-      //  stroke(200,150,random(0,100),2);
-        //line(width,mouseY,mouseX,height/2);
-      //rect(random(width),x,random(50));
+      if(toggle == 2){
+         fill(c3);
+        //stroke(c2);
       }
 
 
-}
+      push();
+      translate(posX,posY);
 
-  var ranNum = int(random(200));
-  if(ranNum == 6){
-    background(255);
+      beginShape();
+      vertex(shiftX,shiftY);
+      vertex(rectSize + shiftX2,shiftY2);
+      vertex(rectSize + shiftX3,rectSize + shiftY3);
+      vertex(shiftX4, rectSize + shiftY4);
+      vertex(rectSize + shiftX5 -20, rectSize + shiftY5);
+            vertex(shiftX4, rectSize + shiftY4);
+      vertex(shiftX6 ,shiftY6);
+      endShape();
+      pop();
 
+      fill(255);
+      ellipseMode(CENTER);
+      //strokeWeight(0.5);
+      //stroke(183,87,200);
+      noStroke();
+      ellipse(posX+tileCount*2,posY + tileCount*2,10,10);
+
+    }
   }
-  if(ranNum < 10){
 
-    count++;
-  }
-
-}
-
-function mouseClicked() {
-
-    background(255,200);
+  rectMode(CENTER);
+  noFill();
+  strokeWeight(30);
+  stroke(255);
+  rect(width/2,height/2,width/1.5,height/1.5);
 
 }
+
+function mousePressed() {
+  actRandomSeed = random(100000);
+
+  background(0);
+
+ // background(0);
+}
+
+var gd = '2019_11_09_00';
